dcf5841573b29377373178e54479512ae37ba3c7
Ezequiel Castellanos <51804994+EzequielCaste@users.noreply.github.com>
2023-02-09 19:57:28 -0300

    style search button when expanded

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -5,35 +5,42 @@ import LocationInput from './LocationInput'
 
 import styles from './Search.module.css'
 
-const documentBody = document.querySelector('body')!
-//documentBody.style.backgroundColor = 'rgba(79, 79, 79, 0.4)'
+function changeBackground() {
+  const documentBody = document.querySelector('body')!
+
+  if (documentBody.style.backgroundColor !== 'rgba(79, 79, 79, 0.4)') {
+    documentBody.style.backgroundColor = 'rgba(79, 79, 79, 0.4)'
+  } else {
+    documentBody.style.backgroundColor = 'white'
+  }
+}
 
 const Search = () => {
   const [expand, setExpand] = useState(false)
 
   function expandSearch() {
     setExpand(state => !state)
-
-    if (documentBody.style.backgroundColor !== 'rgba(79, 79, 79, 0.4)') {
-      documentBody.style.backgroundColor = 'rgba(79, 79, 79, 0.4)'
-    } else {
-      documentBody.style.backgroundColor = 'white'
-    }
+    changeBackground()    
   }
 
   const drawerStyle = `${styles['drawer']} ${expand && styles['drawer-expanded']}`
-
-  const labelStyle = `${styles['header']} ${expand && styles['header-expanded']}`
-    
+  
   const containerStyle = `${styles['search_container']} ${expand && styles['expanded']}`
+
+  const buttonStyle = `${styles['search-icon']} ${expand && styles['search-icon-expanded']}`
+
+  const svgFill = {
+    fill: `${expand ? '#F2F2F2' : '#EB5757'}`
+  }
   
   return (
     <div onClick={expandSearch} className={containerStyle}>
       <div className={drawerStyle}>
         <LocationInput expand={expand} />
         <GuestsInput expand={expand} />        
-        <div className={styles['search-icon']}>
-          <SearchIcon style={{fill: '#EB5757'}} />
+        <div className={buttonStyle}>
+          <SearchIcon style={svgFill} />
+          {expand && <span>Search</span>}
         </div>      
       </div>
     </div>
