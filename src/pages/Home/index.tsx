c8925697a14338d705c2de6a0fc5c61dec374877
Ezequiel Castellanos <51804994+EzequielCaste@users.noreply.github.com>
2023-02-08 21:16:38 -0300

    change amount of cards to display in Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,24 +3,25 @@ import Card from '../../components/Card'
 import stays from '../../stays.json'
 import styles from './Home.module.css'
 
+const staysToDisplay = stays.slice(0,6)
+
 const Home = () => {
   return (
     <main className={styles['container']}>
       <Header />
-      <main>
+      <>
         <div className={styles['title-container']}>
           <span className={styles['title']}>Stays in Finland</span>
           <span className={styles['subtitle']}>12+ stays</span>
         </div>
         <section className={styles['stays-container']}>
-          {/* map of Stays > Card */}
           {
-            stays.map(stay => (
+            staysToDisplay.map(stay => (
               <Card key={stay.title} stay={stay} />
             ))            
           }
         </section>
-      </main>
+      </>
     </main>
   )
 }
